3f52e33066781592882f0f4a075d68a20b9d6f88
Ab-integral <spagsoo@gmail.com>
2023-03-06 13:49:49 -0800

    task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = "Holberton"
+word_last_2 = "Holberton"
+middle_word = "Holberton"
+print(f"First 3 letters: {word_first_3[:3]}")
+print(f"Last 2 letters: {word_last_2[7:]}")
+print(f"Middle word: {middle_word[1:-1]}")
