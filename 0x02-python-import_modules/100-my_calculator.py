52d1545328b3f57c0960c011126c91a319d68672
Ab-integral <spagsoo@gmail.com>
2023-03-13 07:30:30 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
         if sys.argv[2] not in list(ops.keys()):
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
+            
             a = int(sys.argv[1])
             b = int(sys.argv[3])
 print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
