36d31943a4847e3d123bc119ecb5eeefcd8284fb
Ab-integral <spagsoo@gmail.com>
2023-03-13 07:28:50 -0700

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
             sys.exit(1)
             a = int(sys.argv[1])
             b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
