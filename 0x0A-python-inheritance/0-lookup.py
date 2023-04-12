825784d9f83c3fa2cb7983de4c9294d8bcbd97db
Ab-integral <spagsoo@gmail.com>
2023-04-12 09:34:41 -0700

    fixing tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def lookup(obj):
+"""Defines an object attribute lookup function."""
 
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
     return (dir(obj))
